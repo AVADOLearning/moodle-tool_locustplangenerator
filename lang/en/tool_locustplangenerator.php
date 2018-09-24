a83480b246b90ac6db81318b1910d582d44b1501
Luke Carrier <luke@carrier.im>
2018-09-24 15:00:19 +0100

    Skeleton

---
+++ lang/en/tool_locustplangenerator.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Locust plan generator.
+ *
+ * @author Luke Carrier <luke.carrier@avadolearning.com>
+ * @copyright 2018 AVADO Learning Ltd
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+/** @var string[] $string */
+
+$string['pluginname'] = 'Locust plan generator';
---
+++ version.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Locust plan generator.
+ *
+ * @author Luke Carrier <luke.carrier@avadolearning.com>
+ * @copyright 2018 AVADO Learning Ltd
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+/** @var stdClass $plugin */
+
+$plugin->component = 'tool_locustplangenerator';
+
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->release = '0.1.0';
+$plugin->version = 2018092400;
